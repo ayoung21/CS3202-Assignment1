56dcd1d79fa77998d2a510a12bd384f0279f3c07
Andrew Young <ayoung21@my.westga.edu>
2021-01-31 15:32:54 -0500

    Resharper code cleanup on controls

--- Code/GradeEntryLibrary/GradeEntryLibrary/GradeFeedback.cs
+++ Code/GradeEntryLibrary/GradeEntryLibrary/GradeFeedback.cs
@@ -1,35 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
 
 namespace GradeEntryLibrary
 {
-    public partial class GradeFeedback: UserControl
+
+    /// <summary>
+    ///   GradeFeedback class
+    /// </summary>
+    public partial class GradeFeedback : UserControl
     {
-        const string DEFUALT_TITLE = "Grade";
-        const string TEXT_EXCEPTIONAL = "Exceptional";
-        const string TEXT_ACCEPTABLE  = "Acceptable";
-        const string TEXT_AMATEUR = "Amateur";
-        const string TEXT_UNSATISFACTORY = "Unsatisfactory";
-        const int DEFAULT_EXCEPTIONAL_VALUE = 3;
-        const int DEFAULT_ACCEPTABLE_VALUE = 2;
-        const int DEFAULT_AMATEUR_VALUE = 1;
-        const int DEFAULT_UNSATISFACTORY_VALUE = 0;
-        const int NUMBER_OF_DEFAULT_COMMENT_ROWS = 3;
+        #region Data members
 
-        private int _radioButtonExceptionalValue;
-        private int _radioButtonAcceptableValue;
-        private int _radioButtonAmateurValue;
-        private int _radioButtonUnsatisfactoryValue;
+        private const string DefaultTitle = "Grade";
+        private const string TextExceptional = "Exceptional";
+        private const string TextAcceptable = "Acceptable";
+        private const string TextAmateur = "Amateur";
+        private const string TextUnsatisfactory = "Unsatisfactory";
+        private const int DefaultExceptionalValue = 3;
+        private const int DefaultAcceptableValue = 2;
+        private const int DefaultAmateurValue = 1;
+        private const int DefaultUnsatisfactoryValue = 0;
+        private const int CommentCellIndex = 1;
 
+        private int radioButtonExceptionalValue;
+        private int radioButtonAcceptableValue;
+        private int radioButtonAmateurValue;
+        private int radioButtonUnsatisfactoryValue;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>Gets the maximum points.</summary>
+        /// <value>The maximum points.</value>
+        public int MaxPoints { get; private set; }
+
+        /// <summary>Gets or sets the RadioButton exceptional value.</summary>
+        /// <value>The RadioButton exceptional value.</value>
+        public int RadioButtonExceptionalValue
+        {
+            get => this.radioButtonExceptionalValue;
+            set
+            {
+                this.MaxPoints = value;
+                this.radioButtonExceptionalValue = value;
+                this.radioButtonExceptional.Tag = value;
+                this.radioButtonExceptional.Text = $@"({value}) {TextExceptional}";
+            }
+        }
+
+        /// <summary>Gets or sets the RadioButton acceptable value.</summary>
+        /// <value>The RadioButton acceptable value.</value>
+        public int RadioButtonAcceptableValue
+        {
+            get => this.radioButtonAcceptableValue;
+            set
+            {
+                this.radioButtonAcceptableValue = value;
+                this.radioButtonAcceptable.Tag = value;
+                this.radioButtonAcceptable.Text = $@"({value}) {TextAcceptable}";
+            }
+        }
+
+        /// <summary>Gets or sets the RadioButton amateur value.</summary>
+        /// <value>The RadioButton amateur value.</value>
+        public int RadioButtonAmateurValue
+        {
+            get => this.radioButtonAmateurValue;
+            set
+            {
+                this.radioButtonAmateurValue = value;
+                this.radioButtonAmateur.Tag = value;
+                this.radioButtonAmateur.Text = $@"({value}) {TextAmateur}";
+            }
+        }
+
+        /// <summary>Gets or sets the RadioButton unsatisfactory value.</summary>
+        /// <value>The RadioButton unsatisfactory value.</value>
+        public int RadioButtonUnsatisfactoryValue
+        {
+            get => this.radioButtonUnsatisfactoryValue;
+            set
+            {
+                this.radioButtonUnsatisfactoryValue = value;
+                this.radioButtonUnsatisfactory.Tag = value;
+                this.radioButtonUnsatisfactory.Text = $@"({value}) {TextUnsatisfactory}";
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>Initializes a new instance of the <see cref="GradeFeedback" /> class.</summary>
+        public GradeFeedback()
+        {
+            this.InitializeComponent();
+            this.gradeGroupBox.Text = DefaultTitle;
+
+            this.radioButtonExceptional.Checked = true;
+            this.setDefaultTagsForRadioButtons();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>Occurs when [feedback changed].</summary>
         public event EventHandler<EventArgs> FeedbackChanged;
 
         private void onFeedbackChanged()
@@ -37,15 +116,20 @@ namespace GradeEntryLibrary
             this.FeedbackChanged?.Invoke(this, new EventArgs());
         }
 
+
+        /// <summary>Gets the selected comments.</summary>
+        /// <returns>
+        ///   List of currently selected comments
+        /// </returns>
         public List<string> GetSelectedComments()
         {
             var comments = new List<string>();
             foreach (DataGridViewRow row in this.dataGridViewFeedback.Rows)
             {
-                DataGridViewCheckBoxCell cell = (DataGridViewCheckBoxCell)row.Cells[0];
+                var cell = (DataGridViewCheckBoxCell) row.Cells[0];
                 if (Convert.ToBoolean(cell.EditedFormattedValue))
                 {
-                    DataGridViewTextBoxCell textCell = (DataGridViewTextBoxCell)row.Cells[1];
+                    var textCell = (DataGridViewTextBoxCell) row.Cells[1];
                     comments.Add(this.GetValueFromCell(textCell));
                 }
             }
@@ -53,152 +137,105 @@ namespace GradeEntryLibrary
             return comments;
         }
 
+
+        /// <summary>Gets all comments.</summary>
+        /// <returns>
+        ///   List of ALL comments
+        /// </returns>
         public List<string> GetAllComments()
         {
             var comments = new List<string>();
             foreach (DataGridViewRow row in this.dataGridViewFeedback.Rows)
             {
-                DataGridViewTextBoxCell textCell = (DataGridViewTextBoxCell)row.Cells[1];
+                var textCell = (DataGridViewTextBoxCell) row.Cells[1];
                 comments.Add(this.GetValueFromCell(textCell));
             }
 
             return comments;
         }
 
+
+        /// <summary>Deletes all comments.</summary>
         public void DeleteAllComments()
         {
             this.dataGridViewFeedback.Rows.Clear();
             this.dataGridViewFeedback.Refresh();
         }
 
+        /// <summary>Loads the comments.</summary>
+        /// <param name="comments">The comments.</param>
         public void LoadComments(IList<string> comments)
         {
-            var COMMENT_CELL = 1;
-            this.DefaultComments.Clear();
             this.dataGridViewFeedback.Rows.Clear();
             this.dataGridViewFeedback.Rows.Add(comments.Count() + 1);
 
-            for (int i = 0; i < comments.Count(); i++)
+            for (var i = 0; i < comments.Count(); i++)
             {
-                this.dataGridViewFeedback.Rows[i].Cells[COMMENT_CELL].Value = comments[i];
+                this.dataGridViewFeedback.Rows[i].Cells[CommentCellIndex].Value = comments[i];
             }
         }
 
+        /// <summary>Gets the selected RadioButton.</summary>
+        /// <returns>
+        ///   Selected Radio Button
+        /// </returns>
+        public RadioButton GetSelectedRadioButton()
+        {
+            var checkedButton = this.gradeGroupBox.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked);
+
+            return checkedButton;
+        }
+
+
+        /// <summary>Adds the comment.</summary>
+        /// <param name="comment">The comment.</param>
+        public void AddComment(string comment)
+        {
+            const int COMMENT_CELL = 1;
+            var newRowIndex = this.dataGridViewFeedback.Rows.Add();
+            this.dataGridViewFeedback.Rows[newRowIndex].Cells[COMMENT_CELL].Value = comment;
+        }
+
+        /// <summary>Selects all checkboxes.</summary>
+        public void SelectAllCheckboxes()
+        {
+            foreach (DataGridViewRow row in this.dataGridViewFeedback.Rows)
+            {
+                ((DataGridViewCheckBoxCell) row.Cells[0]).Value = true;
+            }
+        }
+
+        /// <summary>Resets the checkboxes.</summary>
+        public void ResetCheckboxes()
+        {
+            foreach (DataGridViewRow row in this.dataGridViewFeedback.Rows)
+            {
+                ((DataGridViewCheckBoxCell) row.Cells[0]).Value = false;
+            }
+        }
+
+        /// <summary>Gets the value from cell.</summary>
+        /// <param name="cell">The cell.</param>
+        /// <returns>
+        ///   Current value from cell
+        /// </returns>
         private string GetValueFromCell(DataGridViewTextBoxCell cell)
         {
             var cellValue = "";
             if (cell.Value != null)
             {
-                cellValue =  cell.Value.ToString();
+                cellValue = cell.Value.ToString();
             }
 
             return cellValue;
         }
 
-        public RadioButton GetSelectedRadioButton()
-        {
-            var checkedButton = this.gradeGroupBox.Controls.OfType<RadioButton>().
-                FirstOrDefault(r => r.Checked);
-
-            return checkedButton;
-        }
-
-        #region Properties
-        public int MaxPoints { get; private set; }
-        public int RadioButtonExceptionalValue
-        {
-            get => _radioButtonExceptionalValue;
-            set
-            {
-                this.MaxPoints = value;
-                this._radioButtonExceptionalValue = value;
-                this.radioButtonExceptional.Tag = value;
-                this.radioButtonExceptional.Text = $"({value}) {TEXT_EXCEPTIONAL}";
-            }
-        }
-
-        public int RadioButtonAcceptableValue
-        {
-            get => _radioButtonAcceptableValue;
-            set
-            {
-                this._radioButtonAcceptableValue = value;
-                this.radioButtonAcceptable.Tag = value;
-                this.radioButtonAcceptable.Text = $"({value}) {TEXT_ACCEPTABLE}";
-            }
-        }
-
-        public int RadioButtonAmateurValue
-        {
-            get => _radioButtonAmateurValue;
-            set
-            {
-                this._radioButtonAmateurValue = value;
-                this.radioButtonAmateur.Tag = value;
-                this.radioButtonAmateur.Text = $"({value}) {TEXT_AMATEUR}";
-            }
-        }
-
-        public int RadioButtonUnsatisfactoryValue
-        {
-            get => _radioButtonUnsatisfactoryValue;
-            set
-            {
-                this._radioButtonUnsatisfactoryValue = value;
-                this.radioButtonUnsatisfactory.Tag = value;
-                this.radioButtonUnsatisfactory.Text = $"({value}) {TEXT_UNSATISFACTORY}";
-            }
-        }
-
-        private IList<string> DefaultComments { get; set; }
-        #endregion
-
-        public GradeFeedback()
-        {
-            InitializeComponent();
-            this.gradeGroupBox.Text = DEFUALT_TITLE;
-            this.DefaultComments = new List<string>();
-            // this.dataGridViewFeedback.Rows.Add(NUMBER_OF_DEFAULT_COMMENT_ROWS);
-
-            this.radioButtonExceptional.Checked = true;
-            this.setDefaultTagsForRadioButtons();
-        }
-
-        public void AddComment(string comment)
-        {
-            const int COMMENT_CELL = 1;
-            // this.DefaultComments.Add(comment);
-
-            var newRowIndex = this.dataGridViewFeedback.Rows.Add();
-            this.dataGridViewFeedback.Rows[newRowIndex].Cells[COMMENT_CELL].Value = comment;
-            //for (int i = 0; i < this.DefaultComments.Count(); i++)
-            //{
-            //    this.dataGridViewFeedback.Rows[i].Cells[COMMENT_CELL].Value = this.DefaultComments[i];
-            //}
-        }
-
-        public void SelectAllCheckboxes()
-        {
-            foreach (DataGridViewRow row in this.dataGridViewFeedback.Rows)
-            {
-                ((DataGridViewCheckBoxCell)row.Cells[0]).Value = true;
-            }
-        }
-
-        public void ResetCheckboxes()
-        {
-            foreach (DataGridViewRow row in this.dataGridViewFeedback.Rows)
-            {
-                ((DataGridViewCheckBoxCell)row.Cells[0]).Value = false;
-            }
-        }
-
         private void setDefaultTagsForRadioButtons()
         {
-            this.RadioButtonExceptionalValue = DEFAULT_EXCEPTIONAL_VALUE;
-            this.RadioButtonAcceptableValue = DEFAULT_ACCEPTABLE_VALUE; 
-            this.RadioButtonAmateurValue = DEFAULT_AMATEUR_VALUE;
-            this.RadioButtonUnsatisfactoryValue = DEFAULT_UNSATISFACTORY_VALUE;
+            this.RadioButtonExceptionalValue = DefaultExceptionalValue;
+            this.RadioButtonAcceptableValue = DefaultAcceptableValue;
+            this.RadioButtonAmateurValue = DefaultAmateurValue;
+            this.RadioButtonUnsatisfactoryValue = DefaultUnsatisfactoryValue;
         }
 
         private void dataGridViewFeedback_CellEndEdit(object sender, DataGridViewCellEventArgs e)
@@ -220,10 +257,7 @@ namespace GradeEntryLibrary
         {
             this.onFeedbackChanged();
         }
-    }
 
-    public class MessageSentEventArgs : EventArgs
-    {
-        public string Message { get; set; }
+        #endregion
     }
 }
